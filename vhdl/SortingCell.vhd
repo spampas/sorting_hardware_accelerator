6d3b1aaed53901e3796fc8994aa4d0c6ac183c34
spampas <49267419+spampas@users.noreply.github.com>
2020-11-24 12:04:06 +0100

    updates on SortingStage and SortingCell code completed.

--- vhdl/SortingCell.vhd
+++ vhdl/SortingCell.vhd
@@ -22,7 +22,7 @@ end SortingCell;
 
 architecture bhv of SortingCell is
 
-	component sortingcell_comparator_stage 
+	component sortingcell_sorting_stage 
 
 		generic(N: INTEGER := 5);
 		port(
@@ -39,27 +39,35 @@ architecture bhv of SortingCell is
 
 	type data is array (0 to M-1) of std_logic_vector(N-1 downto 0);
 	type flag is array (0 to M-1) of std_logic;
+	type reset is array (0 to M-1) of std_logic;
 
 
+	signal stage_write_enable : std_logic := '0';
 	signal stage_data : data;
 	signal stage_flag : flag;
+	signal stage_rst : reset := (others => '0');
+
+	signal counter : integer := 0;
+	signal actual_output : integer := 0;
+	signal data_requested: boolean := false;
+	signal serving_data_request: boolean := false;
 
 begin 
 	sorting_pipeline: for stage in 0 to M-1 generate
 
 		first_stage: if stage = 0 generate
-			begin frist_stage : sortingcell_comparator_stage port map ( clk => clk,
-									    	rst => rst,
+			begin frist_stage : sortingcell_sorting_stage port map ( clk => clk,
+									    	rst => stage_rst(0),
 									    	received_data => symbol_in,
-									    	received_flag => write_enable,
+									    	received_flag => stage_write_enable,
 									    	forwarded_data => stage_data(stage),
 									    	forward_flag => stage_flag(stage));
 		end generate first_stage;
 
 		last_stage: if stage = M-1 generate
 
-			begin last_stage : sortingcell_comparator_stage port map ( clk => clk,
-									    	rst => rst,
+			begin last_stage : sortingcell_sorting_stage port map ( clk => clk,
+									    	rst => stage_rst(stage),
 									    	received_data => stage_data(stage -1),
 									    	received_flag => stage_flag(stage -1),
 									    	forwarded_data => stage_data(stage));
@@ -67,12 +75,84 @@ begin
 
 		normal_stage : if (stage /= 0) and (stage /= M-1) generate
 
-            		begin regular_cells : sortingcell_comparator_stage port map (   clk => clk,
-                                                            				rst => rst,
+            		begin regular_cells : sortingcell_sorting_stage port map (   clk => clk,
+                                                            				rst => stage_rst(stage),
                                                             				received_data => stage_data(stage -1),
 						           	 			received_flag => stage_flag(stage -1),
 							    				forwarded_data => stage_data(stage),
 						            				forward_flag => stage_flag(stage));
             	end generate normal_stage;
 	end generate sorting_pipeline;
+
+
+
+	rst_setup: for k in 0 to M-1 generate
+		stage_rst(k) <= '1' when( rst = '1' or (serving_data_request and actual_output = k)) else '0';
+	end generate rst_setup;
+
+
+	stage_write_enable <= '1' when ( write_enable = '1' and counter /= M) else '0';
+
+
+
+	
+	counter_state: process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				counter <= 0;
+			else
+				if write_enable = '1' and counter < M then
+					counter <= counter +1;
+				end if;
+				if serving_data_request then
+					counter <= counter -1;
+					actual_output <= actual_output +1;
+				else
+					actual_output <= 0;
+				end if;
+			end if;
+		end if;
+	end process counter_state;
+
+	output_state: process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				data_requested <= false;
+				serving_data_request <= false;
+			else
+				if read_symbols = '1' and counter >0 and write_enable = '1' then
+					data_requested <= true;
+				end if;
+				if read_symbols = '1' and counter >0 and write_enable = '0' then
+					serving_data_request <= true;
+				end if;
+				if data_requested and write_enable = '0' then
+					serving_data_request <= true;
+					data_requested <= false;
+				end if;
+				if counter = 0 then
+					serving_data_request <= false;
+				end if;
+			end if;
+		end if;
+	end process output_state;
+
+
+	output: process(clk)
+		begin
+			if rising_edge(clk) then
+				if rst = '1' then
+				else
+					if serving_data_request and counter >0 and write_enable = '0' then
+						symbol_out <= stage_data(actual_output);
+					end if;
+				end if;
+			end if;
+	end process output;
+
+
+
+
 end bhv;
---
+++ vhdl/SortingStage.vhd
@@ -0,0 +1,91 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+-- This module is a stage of the pipeline that makes the sorting.
+-- The generic N is the number of bits of the unsigned that we want to sort.
+
+--------Sorting Stage Entity ---------------------------------------------------------------
+entity sortingcell_sorting_stage is generic(N: INTEGER := 5);
+	port(
+		received_data: in std_logic_vector(N-1 downto 0);
+		received_flag: in std_logic;
+		
+		forwarded_data: out std_logic_vector(N-1 downto 0);
+		forward_flag: out std_logic;
+
+		clk: in std_logic;
+		rst: in std_logic
+	);
+end sortingcell_sorting_stage;
+
+----------------------------------------------------------------------------------------------
+
+------- Sorting Stage Architecture -----------------------------------------------------------
+architecture bhv of sortingcell_sorting_stage is
+
+	signal empty : boolean := true; -- when empty is true, the stage does not contain any unsigned value
+	signal current_data : std_logic_vector (N-1 downto 0) := (others => '0');
+begin
+
+	state: process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				empty <= true;
+			else
+				if received_flag = '1' then -- a new unsigned is arrived from previous stage
+					empty <= false;
+				end if;
+			end if;
+		end if;
+	end process state;
+
+	data: process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				current_data <= (others => '0');
+				forwarded_data <= (others =>'0');
+			else
+				if received_flag = '1' then
+					case empty is
+						when true =>
+							current_data <= received_data;
+						when false =>
+							if received_data < current_data then
+								forwarded_data <= current_data;
+								current_data <= received_data;
+							else
+								forwarded_data <= received_data;
+							end if;
+					end case;
+				else 
+					forwarded_data <= current_data;
+				end if;
+				
+				
+			end if;
+
+		end if;
+	end process data;
+
+	flag: process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				forward_flag <= '0';
+			else
+				if received_flag = '1' and not empty then
+					forward_flag <= '1';
+				else
+					forward_flag <= '0';
+				end if;	
+			end if;
+
+		end if;
+	end process flag;
+	
+end bhv;
+----------------------------------------------------------------------------------------------------------
+	
