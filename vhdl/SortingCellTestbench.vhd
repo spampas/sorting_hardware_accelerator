471e89ae849e0d2c81f58a0c2bf536ad3deb3b52
spampas <49267419+spampas@users.noreply.github.com>
2020-11-28 17:40:04 +0100

    Update SortingCellTestbench.vhd

--- vhdl/SortingCellTestbench.vhd
+++ vhdl/SortingCellTestbench.vhd
@@ -24,7 +24,7 @@ constant T_CLK : time := 100ns;
 
 --signals
 signal clk : std_logic := '0';
-signal rst : std_logic := '1';
+signal rst : std_logic := '0';
 
 signal write_enable : std_logic := '0';
 signal symbol_in : std_logic_vector(N-1 downto 0);
@@ -67,7 +67,37 @@ begin
 			rst => rst);
 --
 
-	CLOCK: clk <= (not(clk)) after T_CLK/2;
+	CLOCK: clk <= (not(clk)and end_sim) after T_CLK/2;
+
+	d_process : process(clk)
+		variable t : integer := 0;
+	begin
+		if(rising_edge(clk)) then
+			case(t) is
+				when 1 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 2 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(3,8)); read_symbols <= '0';
+				when 3 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(1,8)); read_symbols <= '0';
+				when 4 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(4,8)); read_symbols <= '1';
+				when 5 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(2,8)); read_symbols <= '0';
+				when 6 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(5,8)); read_symbols <= '0';
+				when 7 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 8 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 9 => write_enable <= '0'; symbol_in <= std_logic_vector(to_unsigned(6,8)); read_symbols <= '0';
+				when 10 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(10,8)); read_symbols <= '1';
+				when 11 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(8,8)); read_symbols <= '0';
+				when 12 => write_enable <= '1'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 13 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 14 => write_enable <= '1'; symbol_in <= std_logic_vector(to_unsigned(99,8)); read_symbols <= '0';
+				when 15 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 16 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 17 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+				when 18 => write_enable <= '0'; symbol_in <= (others => '0'); read_symbols <= '0';
+
+				when others => null;
+			end case;
+			t := t+1;
+		end if;
+	end process;
 
 end bhv;
 
